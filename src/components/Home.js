c158cf4bd8ee305892bead6597f2205692e0228b
Nevsimao <coday.ink@gmail.com>
2021-03-15 15:12:55 -0400

    Little changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,31 +12,37 @@ import '../stylesheets/App.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import useSWR from 'swr';
 
+//The center position for the map
 let position = [37.76501750599999, -122.40126092359778];
+
+//SWR is a strategy to first return the data from cache (stale), then send the fetch request (revalidate), and finally come with the up-to-date data.
 const fetcher = (...args) => fetch(...args).then((response) => response.json());
 
 const Home = () => {
 	const url = 'https://data.sfgov.org/resource/nc68-ngbr.json';
 
 	const { data, error } = useSWR(url, { fetcher });
-
+	//Here we extracting a section of a data and returns
 	const places = data && !error ? data.slice(0, 100) : [];
-
+	//useState for the search bar and the ifo returned by the search
 	const [searchText, setSearchText] = useState('');
 	const [Info, setInfo] = useState(places);
-
+	//Getting reade of facility id in the search bar just incase it's similar to an address and we can decrease the time-complexity
 	const excludeColumns = ['facility_id'];
-
+	//Function that handles the input in search bar
 	const handleSubmit = (value) => {
 		setSearchText(value);
 		filterData(value);
 	};
-
+	
+	//Here we will be making sure the data is all changed into lower case and using trim method to remove white spaces
 	const filterData = (value) => {
 		const lowercasedValue = value.toLowerCase().trim();
+		//passing all the props to the info component by updating the state
 		if (lowercasedValue === '') setInfo(places);
 		else {
 			const filteredData = places.filter((item) => {
+				//Here we use object.keys to get all the index into key values
 				return Object.keys(item).some((key) =>
 					excludeColumns.includes(key)
 						? false
@@ -83,6 +89,7 @@ const Home = () => {
 						center={position}
 						zoom={12}
 						scrollWheelZoom={true}
+						debug={false}
 						attributionControl={false}>
 						<TileLayer
 							attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
